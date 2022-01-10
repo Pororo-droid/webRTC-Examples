1a5d69e9e4d2d50a94f9e53d70cd399eceeaa397
Pororo-droid <yjwsis@kookmin.ac.kr>
2022-01-10 13:03:55 +0900

    [ADD] single connection

---
+++ single connection/index.js
@@ -0,0 +1,18 @@
+const mediaStreamConstraints = {
+    video: true,
+};
+
+const localVideo = document.querySelector('video')
+
+let localStream;
+
+function gotLocalMediaStream(mediaStream) {
+    localStream = mediaStream;
+    localVideo.srcObject = mediaStream;
+}
+
+function handleLocalMediaStreamError(error){
+    console.log('error',error);
+}
+
+navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(gotLocalMediaStream).catch(handleLocalMediaStreamError)
